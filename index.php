34dd3babd7007cdd65aec6350bc866de83b9a237
Katherin Williams <96099968+katherinWilliams@users.noreply.github.com>
2023-02-01 01:06:01 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Graphic Design</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" style="color:#348CC9" href="#">Blue Otter Design</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle"  href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Categories
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#invitations">Invitations</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#flyers">Flyers</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#logos">Logos</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#other">Other Designs</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+     
+    <div class="logo">
+      <img src="images/blueOtterLogo2.jpg" class="d-block w-80" alt="Blue Otter Design Logo">
+    </div>   
+
+    <a id="invitations">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">Invitations</h2>
+          </div>
+
+          <div class="container-fluid">
+              <div class="row">
+                  <div class="col-lg-4 col-md-4 col-sm-4 col-12">
+                    <img src="images/flipFlops.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-4 col-12">
+                    <img src="images/christmas.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-4 col-12">
+                    <img src="images/invite2.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-4 scol-12">
+                    <img src="images/cutie.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-4 scol-12">
+                    <img src="images/invite new.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-sm-4 scol-12">
+                    <img src="images/starr_babyshower.jpg" class="img-fluid pb-3">
+                  </div>
+              </div>
+          </div>  
+      </section>
+    </a>
+
+    <a id="flyers">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">Flyers</h2>
+          </div>
+
+          <div class="container-fluid">
+              <div class="row">
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/funRun2018B.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/ponyExpress2.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/carnival2.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/rideDays.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/funRun.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/tolkein2.jpg" class="img-fluid pb-3">
+                  </div>
+              </div>
+          </div>  
+      </section>
+    </a>
+
+    <a id="logos">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">Logos</h2>
+          </div>
+
+          <div class="container-fluid">
+              <div class="row">
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/limeLogo4.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/horseLogo.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/blueOtterLogo3.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/myLogo2.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/tolkien2.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/jt2.jpg" class="img-fluid pb-3">
+                  </div>
+              </div>
+          </div>  
+      </section>
+    </a>
+
+    <a id="other">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">Other Designs</h2>
+          </div>
+
+          <div class="container-fluid">
+              <div class="row">
+                  
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/calendar.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/mayTheHorse.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/T-shirt back.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/auroraSign.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/noTry.jpg" class="img-fluid pb-3">
+                  </div>
+                  <div class="col-lg-4 col-md-4 col-12">
+                    <img src="images/bingo.jpg" class="img-fluid pb-3">
+                  </div>
+                  
+              </div>
+          </div>  
+      </section>
+    </a>
+
+    <a id="contact">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">Contact Me</h2>
+              <h5 class="text-center">If you would like something made, send me your info and we'll talk!</h5>
+          </div>
+
+          <div class="w-50 m-auto">
+            <form action="about.php" method="post">
+              <div class="form-group">
+                <label>Name:</label>
+                <input type="text" name="name" class="form-control"> 
+              </div>
+              <div class="form-group">
+                <label>Email:</label>
+                <input type="email" name="email" placeholder="sample@gmail.com" class="form-control"> 
+              </div>
+              <div class="form-group">
+                <label for="#phone-input">Phone Number:<span class="required"></span></label>
+                <input id="phone-input" type="tel" value="" name="number" placeholder="(555)-555-5555" class="form-control"> 
+              </div>
+              <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+          </div>
+      </section>
+    </a>
+
+    <a id="about">
+      <section class="my-4">
+          <div class="py-4">
+              <h2 class="text-center">About</h2>
+          </div>
+          <div>
+            <img style="margin-left: auto; margin-right: auto; width: 250px; height: 250px" src="images/mickeyMe.jpg" class="d-block w-40" alt="Katherin Williams headshot">
+          </div> 
+          <div class="container-fluid">
+            <h2 class="text-center">Katherin Williams</h2><br>
+            <p class="text-center"><b>I love graphic design and would love to design for you. <br> I have a BS in web developing with a minor in graphic design. <br>
+              I have been designing for 3 years now. These include: non-profits, friends, family, churches, and more.</b></p>
+          </div>
+      </section>
+    </a>
+    
+<!-- Copyright section -->       
+
+    <div class="copyrightSection">
+        <div class="col-md-12 text-center">
+            <p>&copy; Katherin Williams 2022</p>
+        </div>
+    </div>
+
+
+    <script scr="https://ajax.googleapis.com/ajax/libs/jqery/3.5.1/jquery.min.js"></script>
+    <script scr="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
